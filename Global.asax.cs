20c5cead930d2572a14961aa0492084c73215718
Jesusgmrtz <jesus.martinez@acftechnologies.com>
2018-09-17 18:50:03 -0500

    fix2

--- Global.asax.cs
+++ Global.asax.cs
@@ -6,6 +6,7 @@ using Microsoft.Bot.Builder.Azure;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Internals;
 using Microsoft.Bot.Connector;
+using System;
 
 namespace SimpleEchoBot
 {
@@ -23,12 +24,12 @@ namespace SimpleEchoBot
                     builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
 
                     // Using Azure Table Storage
-                    var store = new TableBotDataStore(ConfigurationManager.AppSettings["AzureWebJobsStorage"]); // requires Microsoft.BotBuilder.Azure Nuget package 
+                    //var store = new TableBotDataStore(ConfigurationManager.AppSettings["AzureWebJobsStorage"]); // requires Microsoft.BotBuilder.Azure Nuget package 
 
                     // To use CosmosDb or InMemory storage instead of the default table storage, uncomment the corresponding line below
-                    // var store = new DocumentDbBotDataStore("cosmos db uri", "cosmos db key"); // requires Microsoft.BotBuilder.Azure Nuget package 
-                    // var store = new InMemoryDataStore(); // volatile in-memory store
-
+                    var uri = new Uri(ConfigurationManager.AppSettings["DocumentDbUrl"]);
+                    var key = ConfigurationManager.AppSettings["DocumentDbKey"];// var store = new InMemoryDataStore(); // volatile in-memory store
+                    var store = new DocumentDbBotDataStore(uri, key);
                     builder.Register(c => store)
                         .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
                         .AsSelf()
